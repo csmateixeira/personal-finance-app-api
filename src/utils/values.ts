6f182a99e48734d51f6bed71f163413a29a28334
Carla T <csmateixeira@users.noreply.github.com>
2025-04-07 16:19:27 +0100

    Added public status endpoint

--- src/utils/values.ts
+++ src/utils/values.ts
@@ -1,3 +1,8 @@
+import { SetMetadata } from '@nestjs/common';
+
 export const BUDGETS_REPOSITORY = 'BUDGETS_REPOSITORY';
 export const TRANSACTIONS_REPOSITORY = 'TRANSACTIONS_REPOSITORY';
 export const DATA_SOURCE = 'DATA_SOURCE';
+export const IS_PUBLIC_KEY = 'isPublic';
+
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
