0a8c4b8c853ab8044d47cb8a458692b0b157eef9
Carla T <csmateixeira@users.noreply.github.com>
2025-04-09 14:29:59 +0100

    Added CRUD operations for budgets

--- src/budgets/budgets.service.ts
+++ src/budgets/budgets.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { Budget } from './budgets.entity';
 import { BUDGETS_REPOSITORY } from '../utils/values';
 
@@ -16,5 +16,17 @@ export class BudgetsService {
 
   findByCategory(category: string): Promise<Budget | null> {
     return this.budgetsRepository.findOneBy({ category });
+  }
+
+  findById(id: string): Promise<Budget | null> {
+    return this.budgetsRepository.findOneBy({ id });
+  }
+
+  upsert(budget: Budget): Promise<Budget> {
+    return this.budgetsRepository.save(budget);
+  }
+
+  remove(id: string): Promise<DeleteResult> {
+    return this.budgetsRepository.delete(id);
   }
 }
