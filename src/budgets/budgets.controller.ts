6f87047ac32da4e0a5641fc84100315879917227
Carla T <csmateixeira@users.noreply.github.com>
2025-04-09 14:30:52 +0100

    Added CRUD operations for budgets (#10)

--- src/budgets/budgets.controller.ts
+++ src/budgets/budgets.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { BudgetsService } from './budgets.service';
 import { Budget } from 'src/budgets/budgets.entity';
 import { Response } from 'express';
@@ -38,6 +48,66 @@ export class BudgetsController {
     return {
       status: HttpStatus.OK,
       data: budget,
+    };
+  }
+
+  @Post()
+  async createBudget(
+    @Res({ passthrough: true }) response: Response,
+    @Body() budget: Budget,
+  ): Promise<ApiResponse<Budget>> {
+    const createdBudget: Budget = await this.budgetsService.upsert(budget);
+
+    response.status(HttpStatus.CREATED);
+
+    return {
+      status: HttpStatus.CREATED,
+      data: createdBudget,
+    };
+  }
+
+  @Put(':id')
+  async updateBudget(
+    @Res({ passthrough: true }) response: Response,
+    @Body() budget: Budget,
+    @Param('id') id: string,
+  ): Promise<ApiResponse<Budget>> {
+    const createdBudget: Budget = await this.budgetsService.upsert({
+      ...budget,
+      id,
+    });
+
+    response.status(HttpStatus.ACCEPTED);
+
+    return {
+      status: HttpStatus.ACCEPTED,
+      data: createdBudget,
+    };
+  }
+
+  @Delete(':id')
+  async deleteBudget(
+    @Res({ passthrough: true }) response: Response,
+    @Param('id') id: string,
+  ): Promise<ApiResponse<boolean>> {
+    const budget: Budget | null = await this.budgetsService.findById(id);
+
+    if (budget === null) {
+      response.status(HttpStatus.NOT_FOUND);
+
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: 'Budget not found with id: ' + id,
+      };
+    }
+
+    await this.budgetsService.remove(id);
+
+    response.status(HttpStatus.ACCEPTED);
+
+    return {
+      status: HttpStatus.ACCEPTED,
+      data: true,
     };
   }
 }
